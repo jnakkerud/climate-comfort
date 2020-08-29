914e80a4b479a59962414ca0a6bdec560dbb0a2a
Jon Nakkerud <jonnakkerud@Jons-MacBook-Air.local>
2020-08-29 16:41:44 -0700

    Output scores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 import { ClimateScoreService } from './climate-score.service';
 
+export interface Card {
+  network: string;
+  station: string;
+  // scores
+  [index: string]: number | string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,14 +20,26 @@ export class AppComponent {
   network = 'CA_ASOS';
   station = 'OAK';
 
-  constructor(private climateScoreService: ClimateScoreService) {}
+  cards: Card[] = [];
+
+  constructor(private climateScoreService: ClimateScoreService) { }
 
   onClick(): void {
-    // load the data
 
-    this.climateScoreService.score(this.network, this.station)
-    .then(res => {
-      console.log('result', res);
+    // load the data
+    this.climateScoreService.score(this.network, this.station).then(res => {
+
+      const card: Card = {
+        network: this.network,
+        station: this.station
+      };
+
+      for (const prop of res) {
+        card[prop[0]] = prop[1];
+      }
+
+      this.cards.push(card);
+
     }, err => console.log(err));
   }
 
