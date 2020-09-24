6d34b53791a89e881db974d7713e6110d724cbcd
Jon Nakkerud <jonnakkerud@Jons-MacBook-Air.local>
2020-09-24 11:32:34 -0700

    pixel polish autocomplete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import { ClimateScoreService } from './climate-score.service';
 import { StationService, Station } from './station.service';
 
 export interface Card {
-    network: string;
     station: string;
+    network: string;
+    location: string;
     // scores
-    [index: string]: number | string;
+    scores: Map<string, number>;
 }
 
 @Component({
@@ -25,14 +26,14 @@ export class AppComponent implements OnInit {
 
     filteredStations: Observable<Station[]>;
 
-    myForm = new FormGroup({
+    criteriaForm = new FormGroup({
         search: new FormControl(null)
     });
 
     constructor(private climateScoreService: ClimateScoreService, private stationService: StationService) { }
 
     ngOnInit(): void {
-        this.filteredStations = this.myForm.get('search').valueChanges
+        this.filteredStations = this.criteriaForm.get('search').valueChanges
             .pipe(
                 debounceTime(400),
                 switchMap(value => from(this.stationService.search(value)))
@@ -40,22 +41,19 @@ export class AppComponent implements OnInit {
     }
 
     private score(station: Station): void {
-
         // load the data
-        this.climateScoreService.score(station).then(res => {
+        this.climateScoreService.score(station).then(scoresResult => {
 
             const card: Card = {
+                station: station.station_name,
                 network: station.station_type,
-                station: station.station_name
+                location: `https://www.openstreetmap.org/?mlat=${station.lat_dec}&mlon=${station.lon_dec}`,
+                scores: scoresResult
             };
-
-            for (const prop of res) {
-                card[prop[0]] = prop[1];
-            }
 
             this.cards.push(card);
 
-            this.clearSelections();
+            this.reset();
 
         }, err => console.log(err));
     }
@@ -68,7 +66,7 @@ export class AppComponent implements OnInit {
         return station ? station.station_name : undefined;
     }
 
-    private clearSelections(): void {
-        this.myForm.get('search').reset(null, {emitEvent: false});
+    private reset(): void {
+        this.criteriaForm.get('search').reset(null, {emitEvent: false});
     }
 }
